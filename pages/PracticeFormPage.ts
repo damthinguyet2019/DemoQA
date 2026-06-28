7b3e54477f6bb3e9601257b8bb4403aa4b5573d4
nguyetdt <nguyetdt@onepay.vn>
2026-06-28 16:55:41 +0700

    day10

--- pages/PracticeFormPage.ts
+++ pages/PracticeFormPage.ts
@@ -9,28 +9,27 @@ export class PracticeFormPage {
   readonly txtMobile;
   readonly txtDateOfBirth;
   readonly txtSubjects;
+  //khai báo để truyền nhập động, khong can khai bao trong contructor nua
   chkHobbies:string='//label[text()="@param"]';
-  
- // readonly fileUpload;
+  readonly txtPicture;
   readonly txtCurrentAddress;
-  readonly lblState;
-  readonly lblCity;
+  readonly cbState;
+  readonly cbCity;
   readonly btnSubmit;
 
   constructor(public readonly page: Page) {
     this.txtFirstName = page.locator("#firstName");
     this.txtLastName = page.locator("#lastName");
     this.txtEmail = page.locator("#userEmail");
-
-
     this.txtMobile = page.locator("#userNumber")
     this.rdoOther = page.locator("#gender-radio-3");
     this.txtDateOfBirth = page.locator("#dateOfBirthInput");
     this.txtSubjects = page.locator("#subjectsInput");
-    //this.btnChooseFile = page.locator("#uploadPicture");
+    this.txtPicture = page.locator("#uploadPicture");
     this.txtCurrentAddress = page.locator("#currentAddress");
-    this.lblState = page.locator("#state");
-    this.lblCity = page.locator("#city");
+    this.cbState = page.locator('xpath= //*[@id="state"]//input');
+    this.cbCity = page.locator('xpath= //*[@id="city"]//input');
+    
     this.btnSubmit = page.locator("#submit");
   }
 
@@ -49,6 +48,8 @@ export class PracticeFormPage {
     picture:string,
     subject:string,
     hobbies:string,
+    state:string,
+    city:string,
      ) 
      {  
     await this.txtFirstName.fill(firstName);
@@ -57,12 +58,18 @@ export class PracticeFormPage {
     await this.page.click(this.rdGender.replace('@param', gender));
     await this.txtMobile.fill(mobile);
     await this.inputDateOfBirth(dateOfBirth);
-    await this.txtSubjects.fill(subject);
-    
+    await this.inputSubjects(subject);  // gọi hàm nhập từ async bên dưới
     await this.inputDateOfBirth(dateOfBirth);
-    
+    await this.inputHobbies(hobbies);
+    //truyền đường dẫn thư mục dự án để lấy tên file ảnh truyền vào
+  const picturePath = process.cwd()+'/testcase/data/' + picture; // thu muc chua data du an
+ await this.txtPicture.fill(picturePath);
     await this.txtCurrentAddress.fill(currentAddress);
-        
+    await this.cbState.fill(state);
+    await this.cbState.press('Enter')
+ await this.cbCity.fill(city);
+        await this.cbCity.press('Enter')
+
     await this.btnSubmit.click();
   }
 
@@ -92,6 +99,7 @@ async getLocatorByText(originalXpath: string, text: string): Promise<Locator> {
         await this.page.locator('.react-datepicker__month-select').selectOption(month);
         await this.page.locator(`.react-datepicker__day--0${day}`).click();
     }
+    // hàm nhập lấy data từ combobox, input sau click
     async inputSubjects(subjects: string) {
         const subjectList = subjects.split(',').map(subject => subject.trim());
         for (const subject of subjectList) {
@@ -99,12 +107,16 @@ async getLocatorByText(originalXpath: string, text: string): Promise<Locator> {
             await this.page.keyboard.press('Enter');
         }
     }
-    // async inputHobbies(hobbies: string) {
-    //     const hobbyList = hobbies.split(',').map(hobby => hobby.trim());  
-    //     for (const hobby of hobbyList) {
-    //         await 
-    //     } 
-    //   }
+
+    //hàm lấy data từ checkbox, tick chọn  
+    async inputHobbies(hobbies: string) {
+        const hobbyList = hobbies.split(',').map(hobby => hobby.trim());  
+        for (const hobby of hobbyList) {
+            await this.page.click(this.chkHobbies.replace('@param',hobby));
+        } 
+      }
+
+
 }
 
 
